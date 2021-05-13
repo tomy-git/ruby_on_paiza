8e3411a019581732577335b082b4ac27f2ff6c3a
WataruTomita <tomi.symphonia@gmail.com>
2021-05-13 16:24:59 +0900

    souvenir

---
+++ lesson/souvenir.rb
@@ -0,0 +1,12 @@
+puts "広さを入力..."
+nagi = gets.to_s.chomp
+puts nagi + "平米にスーベニア"
+# if nagi == 14
+#   puts "14平米にスーベニア"
+# elsif nagi < 14
+#   puts "流石の凪にも狭すぎますね。"
+# elsif nagi > 14
+#   puts "何でもかんでも広ければいいのでしょうか？と、凪は考えます。"
+# else
+#   puts "いぇぇぇぇええええぇぇぇぇぃ（消沈）"
+# end
