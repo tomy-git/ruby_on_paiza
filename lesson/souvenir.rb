bfed82af4fd861dbe1d82a6566742da3ff379abd
WataruTomita <tomi.symphonia@gmail.com>
2021-05-15 12:20:00 +0900

    souvenir.rb if

--- lesson/souvenir.rb
+++ lesson/souvenir.rb
@@ -1,12 +1,13 @@
-puts "広さを入力..."
-nagi = gets.to_s.chomp
-puts nagi + "平米にスーベニア"
-# if nagi == 14
-#   puts "14平米にスーベニア"
-# elsif nagi < 14
-#   puts "流石の凪にも狭すぎますね。"
-# elsif nagi > 14
-#   puts "何でもかんでも広ければいいのでしょうか？と、凪は考えます。"
-# else
-#   puts "いぇぇぇぇええええぇぇぇぇぃ（消沈）"
-# end
+puts "広さを入力...1"
+hirosa = gets.to_s.chomp
+puts hirosa + "平米にスーベニア"
+
+puts "広さを入力...2"
+nagi = gets.to_i
+if nagi == 14
+  puts "14平米にスーベニア"
+elsif nagi < 14
+  puts "流石の凪にも狭すぎますね。"
+else nagi > 14
+  puts "何でもかんでも広ければいいのでしょうか？と、凪は考えます。"
+end
